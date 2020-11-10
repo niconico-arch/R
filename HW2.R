8f15d65911d9f4922a1b6e317872cd123d386221
niconico-arch <71493170+niconico-arch@users.noreply.github.com>
2020-11-10 19:52:21 +0800

    Add files via upload

--- HW2.R
+++ HW2.R
@@ -18,21 +18,23 @@ MVP <- function(mu, Sigma, upper, lmd) {
   return(w)
 }
 
-
-l = rep(0, 50)
+i = seq(1,50,1)
 S = rep(NA, 50)
 result <- list()
 
 for (d in dataset10[1:5]){
+  d <- dataset10$`dataset 1`
   X_log <- diff(log(d$adjusted))[-1]
   N <- ncol(X_log)
   mu <- colMeans(X_log)
   Sigma <- cov(X_log)
-  u = rep(1, 50)
+  u = seq(1/N, 1, by=(1-1/N)/50)
+  l = seq(0, 100, by=2)
   for (i in 1:length(u)){
     w_ <- MVP(mu, Sigma, u[i], l[i])
     ret <- xts(X_log %*% as.numeric(w_), index(X_log))
     S[i] <- SharpeRatio(R=ret, FUN = "StdDev")
   }
-  result <- c(result, cbind(l, u, S))
+  a <-cbind(i, l, u, S)
+  result <- append(result, a)
 }
